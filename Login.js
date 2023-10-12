6595eef759c17dd13851e1464e0b21ec0f988a85
jmracines3 <jmracines3@gmail.com>
2023-10-13 01:38:05 +0800

    updated the file

--- Login.js
+++ Login.js
@@ -3,8 +3,7 @@ function Login() {
     return (
       <div>
         <button onClick={() => alert('Hello, User1')}>Hello User1</button>
-        <button onClick={() => alert('Hello, User1')}>Hello User1</button>'
-        <button onClick={() => alert('Hello, User1')}>Hello User1</button>'
+        <button onClick={() => alert('Hello, User2')}>Hello User1</button>'
       </div>
     );
   }
