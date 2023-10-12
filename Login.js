24a868647a42b15eded63b201acea4960ff05887
ajarwee <ajarwee@byui.edu>
2023-10-12 11:13:37 -0600

    Yes

--- Login.js
+++ Login.js
@@ -4,6 +4,7 @@ function Login() {
       <div>
         <button onClick={() => alert('Hello, User1')}>Hello User1</button>
         <button onClick={() => alert('Hello, User2')}>Hello User2</button>
+        <button onClick={() => alert('Hello, User3')}>Hello User3</button>
       </div>
     );
   }
