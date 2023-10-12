fc750028aab4ef55c4c6357c5c680de1286cb0d9
jmracines3 <jmracines3@gmail.com>
2023-10-13 01:55:58 +0800

    added my button

--- Login.js
+++ Login.js
@@ -1,9 +1,34 @@
-// Login.js
-function Login() {
-    return (
-      <div>
-        <button onClick={() => alert('Hello, User1')}>Hello User1</button>
-        <button onClick={() => alert('Hello, User2')}>Hello User1</button>'
-      </div>
+import React, { useState } from 'react';
+import {StyleSheet, View, Text, Button} from 'react-native';
+
+export default function Login(props){
+    const [message, setMessage] = useState('');
+
+    const sayHello = (name) => {
+        setMessage(`Hello ${name}`);
+    }
+
+    return(
+        <View style={styles.login}>
+            <Text>This is the Login Screen</Text>
+            <Button title="Say Hello" onPress={() => sayHello('User1')}></Button>
+            <Text>{message}</Text>
+        </View>
+
     );
-  }
+}
+
+const styles = StyleSheet.create({
+    login: {
+        flexDirection: 'row',
+        width: '100%',
+        justifyContent: 'space-between',
+        backgroundColor: 'green',
+        height: '12%',
+        alignItems: 'flex-end',
+        paddingBottom: 5,
+        paddingLeft: 10,
+        paddingRight: 10,
+        
+      },
+})
