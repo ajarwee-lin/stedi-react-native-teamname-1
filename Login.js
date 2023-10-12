337fd7c54c1da768f20dedc972c6a5d1854518f0
ajarwee <ajarwee@byui.edu>
2023-10-12 11:09:18 -0600

    Team

--- Login.js
+++ Login.js
@@ -3,8 +3,6 @@ function Login() {
     return (
       <div>
         <button onClick={() => alert('Hello, User1')}>Hello User1</button>
-        <button onClick={() => alert('Hello, User2')}>Hello User2</button>
-        <button onClikc={() => alert('Hello, User3')}>Hello User3</button>
       </div>
     );
   }
